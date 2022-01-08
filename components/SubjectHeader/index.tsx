c4af663503bbf7c925d88b0067c65298a8069a11
abhikumar98 <abhishekkumar35962@gmail.com>
2022-01-08 16:13:19 +0530

    fix: minor ui fix

--- components/SubjectHeader/index.tsx
+++ components/SubjectHeader/index.tsx
@@ -10,18 +10,20 @@ const SubjectHeader = () => {
 	};
 
 	return (
-		<div className="p-4 rounded-md bg-messageHover transition-all ease-in-out mb-4 flex md:items-center justify-between">
-			<ArrowSmLeftIcon
-				onClick={handleGoBack}
-				className="h-8 w-8 text-primaryText mr-4 cursor-pointer"
-			/>
-			<div className="flex flex-col md:flex-row w-full justify-between">
-				<div className=" text-primaryText text-base md:text-xl font-semibold">
-					Hey bro whats up with the BYAC NFT?
-				</div>
-				<div className="mr-4 mt-2 md:mt-0 text-primaryText flex items-center space-x-2 md:space-x-2 text-xs md:text-base">
-					<div className="bg-blue-500 rounded-full h-6 w-6 md:h-8 md:w-8 "></div>
-					<div>bhaisaab.eth</div>
+		<div className="bg-secondaryBackground sticky -top-4 -mt-4 py-4">
+			<div className="p-4 rounded-md bg-messageHover transition-all ease-in-out flex md:items-center justify-between">
+				<ArrowSmLeftIcon
+					onClick={handleGoBack}
+					className="h-8 w-8 text-primaryText mr-4 cursor-pointer"
+				/>
+				<div className="flex flex-col md:flex-row w-full justify-between">
+					<div className=" text-primaryText text-base md:text-xl font-semibold">
+						Hey bro whats up with the BYAC NFT?
+					</div>
+					<div className="mr-4 mt-2 md:mt-0 text-primaryText flex items-center space-x-2 md:space-x-2 text-xs md:text-base">
+						<div className="bg-blue-500 rounded-full h-6 w-6 md:h-8 md:w-8 "></div>
+						<div>bhaisaab.eth</div>
+					</div>
 				</div>
 			</div>
 		</div>
